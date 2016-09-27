7ed267887d7c71c24869fa691cb5f0b9b6debdbc
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2016-09-26 23:03:19 -0500

    Added few more adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ var SCOPES = ["https://www.googleapis.com/auth/spreadsheets",
 	"https://www.googleapis.com/auth/drive.appdata",
 	"https://www.googleapis.com/auth/drive.file",
 	"https://www.googleapis.com/auth/drive.metadata",
-	"https://www.googleapis.com/auth/drive.metadata.readonly",
-	"https://www.googleapis.com/auth/drive.photos.readonly",
-	"https://www.googleapis.com/auth/drive.readonly"
+	//"https://www.googleapis.com/auth/drive.metadata.readonly",
+	//"https://www.googleapis.com/auth/drive.photos.readonly",
+	//"https://www.googleapis.com/auth/drive.readonly"
 ];
 var bibstring = "";
 function checkAuth() {
